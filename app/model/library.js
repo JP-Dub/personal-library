b597f2ee0e6be8dd0344368cb11d87c9f920d05d
Glitch (brash-carnation) <none>
2019-02-07 20:29:11 +0000

    👫🏫 Checkpoint ./app/model/library.js:553280/170

--- app/model/library.js
+++ app/model/library.js
@@ -4,7 +4,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Library = new Schema({
-
+  book : {
+    title : String,
+    comments : [],
+    commentcount : Number 
+  },
+  totalcount : Number 
 });
 
 Library.set( 'toObject', {retainKeyOrder: true});
